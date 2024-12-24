9714e4eb31c73b8826d883c34141cd1304088b31
RagimovaMadina <madinaragimova03@gmail.com>
2024-12-25 02:13:07 +0500

    Add project files.

---
+++ TodoServerApp/Data/ForumItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoServerApp.Data
+{
+    public class ForumItem
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; } //тема вопроса
+        public string? Question { get; set; } //вопрос
+        public string? Answer { get; set; } //ответ
+    }
+}
---
+++ TodoServerApp/Data/Services/MemoryDataService.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using TodoServerApp.Components.Pages;
+using TodoServerApp.Data.Interfaces;
+
+namespace TodoServerApp.Data.Services
+{
+    public class MemoryDataService : IDataService
+    {
+        private static IEnumerable<ForumItem> Forums { get; } = [
+            new() { Id = 1, Name="тема", Question="вопрос", Answer="ответ"},
+            new() { Id = 2, Name="тема", Question="вопрос", Answer="ответ"},
+            new() { Id = 3, Name="тема", Question="вопрос", Answer="ответ"},
+        ];
+
+        public async Task<IEnumerable<ForumItem>> GetForumItemsAsync()
+        {
+            await Task.Delay(1000);
+            return await Task.FromResult(Forums);
+        }
+    }
+}
